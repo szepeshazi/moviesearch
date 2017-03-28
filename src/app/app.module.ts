f600f5c6016e20d17ca5c37afdfc604a06cffdbe
András Szepesházi <szepeshazi@gmail.com>
2017-03-28 20:53:59 +0200

    fix: add animations import to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { MdToolbarModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 
 @NgModule({
@@ -14,7 +16,8 @@ import { BrowserModule } from '@angular/platform-browser';
 		AppRoutingModule,
 		BrowserModule,
 		HttpModule,
-		MdToolbarModule
+		MdToolbarModule,
+		BrowserAnimationsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
